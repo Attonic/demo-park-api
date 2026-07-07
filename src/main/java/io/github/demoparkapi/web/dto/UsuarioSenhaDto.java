90058c09a7731dd806fe0ddedc3f1e405daca6f5
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-07 14:17:12 -0300

    Implementa dto para alteração de senha do usuario

---
+++ src/main/java/io/github/demoparkapi/web/dto/UsuarioSenhaDto.java
@@ -0,0 +1,17 @@
+package io.github.demoparkapi.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class UsuarioSenhaDto {
+
+    private String senhaAtual;
+    private String novaSenha;
+    private String confirmaSenha;
+
+
+}
