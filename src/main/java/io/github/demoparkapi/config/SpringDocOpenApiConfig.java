be46cc4c19cd89076e76e0293f62f3f4ae3e9951
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-10 13:21:30 -0300

    implementa doc api swagger para o projeto

---
+++ src/main/java/io/github/demoparkapi/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,27 @@
+package io.github.demoparkapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return  new OpenAPI()
+                .info(
+                        new Info()
+                                .title("API REST - Spring Park")
+                                .version("v1")
+                                .description("Api de gestão de estacionamento de veículos.")
+                                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Codeacle").email("codeacle@gmail.com"))
+                );
+    }
+
+
+}
