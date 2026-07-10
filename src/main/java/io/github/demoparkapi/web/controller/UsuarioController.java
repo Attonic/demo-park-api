2643572b8603720b5e815521427a0ac08983ce85
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-10 14:05:19 -0300

    documenta com swagger o recurso de cadastrar usuario

--- src/main/java/io/github/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/io/github/demoparkapi/web/controller/UsuarioController.java
@@ -6,10 +6,17 @@ import io.github.demoparkapi.web.dto.UsuarioCreateDto;
 import io.github.demoparkapi.web.dto.UsuarioMapper;
 import io.github.demoparkapi.web.dto.UsuarioResponseDto;
 import io.github.demoparkapi.web.dto.UsuarioSenhaDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.utils.SchemaUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,22 +26,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 import java.util.List;
 
+@Tag(name = "Usuários", description = "Contém todas as operações relativas aos recursos de usuários.")
 @RestController
 @RequestMapping("api/v1/usuarios")
 @RequiredArgsConstructor
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final SchemaUtils schemaUtils;
+    private final ContentNegotiatingViewResolver contentNegotiatingViewResolver;
 
+    @Operation(
+            summary = "Criar um novo usuário.",
+            responses = {
+            @ApiResponse(responseCode = "201",
+                    description = "Recurso Criado com Sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioResponseDto.class))),
+            @ApiResponse(responseCode = "409",
+                    description = "Usuário e-mail já cadastrado no sistema",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "402",
+                        description = "Recurso não processado por dados de entrada inválidos",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @PostMapping
     public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto){
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
+    @Operation(summary = "Busca usuário por ID.")
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDto> findById(@PathVariable Long id) {
         Usuario usuario = usuarioService.buscarPorId(id);
